4fb90412a16b982c4c8ffb14e9a9027480d3e9e6
Anitasilvi <anitaferdina06@gmail.com>
2024-01-08 07:16:47 +0700

    jest CLI

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,16 @@
 import { sum } from "../src/sum";
 
-test("sum(1, 2) must be 3", () => {
+test("test sum function 1", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
+});
+
+test("test sum function 2", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
+});
+
+test("test sum function 3", () => {
   const result = sum(1, 2);
   expect(result).toBe(3);
 });
