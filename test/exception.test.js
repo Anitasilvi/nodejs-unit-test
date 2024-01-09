3ec41f5e240fdc79e3b9a28fe21796397e5964a0
Anitasilvi <anitaferdina06@gmail.com>
2024-01-09 20:37:32 +0700

    code coverage

--- test/exception.test.js
+++ test/exception.test.js
@@ -5,3 +5,7 @@ test("exception", () => {
   expect(() => callMe("Anita")).toThrow(MyException);
   expect(() => callMe("Anita")).toThrow("Ups my exception happen");
 });
+
+test("exception not happens", () => {
+  expect(callMe("Budi")).toBe("OK");
+});
---
+++ vendor/contoh.js
@@ -0,0 +1,3 @@
+function testVendor() {
+  console.info("Hello Vendor");
+}
