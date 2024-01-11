3ebad69e1238ad0587d3bd1271fdf27953d8a042
Anitasilvi <anitaferdina06@gmail.com>
2024-01-11 21:56:40 +0700

    mock partial class

---
+++ test/mock-partial-class.test.js
@@ -0,0 +1,21 @@
+import { UserRepository } from "../src/user-repository.js";
+import { UserService } from "../src/user-service.js";
+
+const repository = new UserRepository();
+const service = new UserService(repository);
+
+test("test mock class findById", () => {
+  const user = { id: 1, name: "Anita" };
+  const findByIdMock = jest.spyOn(repository, "findById");
+  findByIdMock.mockReturnValueOnce(user);
+
+  expect(service.findById(1)).toEqual(user);
+  expect(repository.findById).toHaveBeenCalled();
+  expect(repository.findById).toHaveBeenCalledWith(1);
+  expect(findByIdMock).toHaveBeenCalled();
+  expect(findByIdMock).toHaveBeenCalledWith(1);
+});
+
+test.failing("test mock partial findAll", () => {
+  service.findAll();
+});
